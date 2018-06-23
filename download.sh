798aa76f9b17cbae956405624e36a85adec44ec6
cjhahaha <cj_lee_canu@hotmail.com>
2018-06-23 13:03:53 +0800

    add run.lua

--- download.sh
+++
@@ -1,2 +0,0 @@
-wget http://esat.kuleuven.be/~dneven/data/fastSceneSegmentation/cityscapesSegmentation.t7 -P ./models/
-wget http://esat.kuleuven.be/~dneven/data/fastSceneSegmentation/fastSceneSegmentationFinal.t7 -P ./models/
